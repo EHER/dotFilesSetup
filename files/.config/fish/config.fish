b75ebcfdb9d79661188df49c32d2c1b1be20afe3
Eher Werkspot <alexandre.eher@werkspot.nl>
2017-05-25 10:04:27 +0200

    Remove fish config

--- files/.config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-. $HOME/.config/fish/functions/fish_prompt.fish
-. $HOME/.config/fish/functions/with.fish
-. $HOME/.config/fish/path.fish
-. $HOME/.config/fish/set.fish
-. $HOME/.config/fish/vi-mode.fish
--- files/.config/fish/set.fish
+++
@@ -1,1 +0,0 @@
-set -x HOST werkspot.com
